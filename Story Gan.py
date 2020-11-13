3e3cf0f90bdf86026f525164c621e92f98d28479
Zachmine77 <zachmine77likeszucchini@gmail.com>
2020-11-12 21:33:13 -0500

    Word for word generation and more stuff.
    Fixed - Entries are word for word: Entries will be more random after every few merges depending on the variable used with the merge function.

--- Story Gan.py
+++ Story Gan.py
@@ -1,16 +1,36 @@
+#Fixed - Entries are word for word: Entries will be more random after every few merges depending on the variable used with the merge function.
+
+### Stuff you can change ###
+length    = 20  #This is not the number of words, it is the number of merges.
+############################
+
 import random
+import math
+import warnings
 dataIn = open("data.txt", 'r').read().casefold().split()
 #dataOut= open("Stored.txt",'w')
-def mergeA(lst1, lst2): 
-    lst3 = [value for value in lst1 if value in lst2]
-    return lst3
+
+lst1 = ""
+def mergeA(b, lst1, lst2 = lst1):
+  c = round(b*math.ceil(b/1))
+  a = random.randint(1,c)
+  lst3 = [value for value in lst1 if value in lst2]
+  if a != b:
+    lst3 = lst1 + lst2
+  return lst3
 learnDict = {}
 dataLen = len(dataIn)
 index = 0
 j = 0
-length = 30
 sentance = ""
 succsess = False
+storyDone = False
+errorNum = 0
+g = ""
+
+if 500 > len(dataIn):
+  warnings.warn("Small ammounts of traning will result in less than desireable results!")
+
 while index < len(dataIn):
   word = dataIn[index]
   try:
@@ -35,73 +55,81 @@ while index < len(dataIn):
     except:
       learnDict[str(word + "¶¶¶¶¶")] = str(dataIn[index+5])
   except:
-    print("Error handled.")
+    print("Leaning Basics! Just a moment...")
   index = index + 1
 
-while succsess == False:
-  try:
-    word1 = random.choice(dataIn)
+print("Time to write...")
+
+while storyDone == False:
+  while succsess == False:
     try:
+      word1 = random.choice(dataIn)
       word2 = random.choice(learnDict[word1+"¶"].split())
+      try:
+        word3 = random.choice(mergeA(learnDict[word2+"¶"].split(),
+        learnDict[word1+"¶¶"].split()))
+      except:
+        word3 = random.choice(learnDict[word2+"¶"].split())
+      try:
+        word4 = random.choice(mergeA(learnDict[word3+"¶"].split(),
+        mergeA(learnDict[word2+"¶¶"].split(),
+        learnDict[word1+"¶¶¶"].split())))
+      except:
+        word4 = random.choice(learnDict[word3+"¶"].split())
+      try:
+        word5 = random.choice(mergeA(learnDict[word4+"¶"].split(),
+        mergeA(learnDict[word3+"¶¶"].split(),
+        mergeA(learnDict[word2+"¶¶¶"].split(),
+        learnDict[word1+"¶¶¶¶"].split()))))
+      except:
+        word5 = random.choice(learnDict[word4+"¶"].split())
+      succsess = True
     except:
-      word2 = learnDict[word1+"¶"]
-    try:
-      word3 = random.choice(mergeA(learnDict[word2+"¶"].split(),
-      learnDict[word1+"¶¶"].split()))
-    except:
-      word3 = learnDict[word1+"¶"]
-    try:
-      word4 = random.choice(mergeA(learnDict[word3+"¶"].split(),
-      mergeA(learnDict[word2+"¶¶"].split(),
-      learnDict[word1+"¶¶¶"].split())))
-    except:
-      word4 = learnDict[word1+"¶"]
-    try:
-      word5 = random.choice(mergeA(learnDict[word4+"¶"].split(),
-      mergeA(learnDict[word3+"¶¶"].split(),
-      mergeA(learnDict[word2+"¶¶¶"].split(),
-      learnDict[word1+"¶¶¶¶"].split()))))
-    except:
-      word5 = learnDict[word1+"¶"]
-
+      succsess = False
+  
+  try:
     while j < length:
-
-      word1 = random.choice(mergeA(learnDict[word5+"¶"].split(),
-      mergeA(learnDict[word4+"¶¶"].split(),
-      mergeA(learnDict[word3+"¶¶¶"].split(),
-      mergeA(learnDict[word2+"¶¶¶¶"].split(),
+      word1 = random.choice(
+      mergeA(1,learnDict[word5+"¶"].split(),
+      mergeA(1,learnDict[word4+"¶¶"].split(),
+      mergeA(1,learnDict[word3+"¶¶¶"].split(),
+      mergeA(1,learnDict[word2+"¶¶¶¶"].split(),
       learnDict[word1+"¶¶¶¶¶"].split())))))
 
-      word2 = random.choice(mergeA(learnDict[word5+"¶¶"].split(),
-      mergeA(learnDict[word4+"¶¶¶"].split(),
-      mergeA(learnDict[word3+"¶¶¶¶"].split(),
-      mergeA(learnDict[word2+"¶¶¶¶¶"].split(),
+      word2 = random.choice(
+      mergeA(1,learnDict[word5+"¶¶"].split(),
+      mergeA(1,learnDict[word4+"¶¶¶"].split(),
+      mergeA(1,learnDict[word3+"¶¶¶¶"].split(),
+      mergeA(1,learnDict[word2+"¶¶¶¶¶"].split(),
       learnDict[word1+"¶"].split())))))
 
-      word3 = random.choice(mergeA(learnDict[word5+"¶¶¶"].split(),
-      mergeA(learnDict[word4+"¶¶¶¶"].split(),
-      mergeA(learnDict[word3+"¶¶¶¶¶"].split(),
-      mergeA(learnDict[word2+"¶"].split(),
+      word3 = random.choice(
+      mergeA(1,learnDict[word5+"¶¶¶"].split(),
+      mergeA(1,learnDict[word4+"¶¶¶¶"].split(),
+      mergeA(1,learnDict[word3+"¶¶¶¶¶"].split(),
+      mergeA(1,learnDict[word2+"¶"].split(),
       learnDict[word1+"¶¶"].split())))))
 
-      word4 = random.choice(mergeA(learnDict[word5+"¶¶¶¶"].split(),
-      mergeA(learnDict[word4+"¶¶¶¶¶"].split(),
-      mergeA(learnDict[word3+"¶"].split(),
-      mergeA(learnDict[word2+"¶¶"].split(),
+      word4 = random.choice(
+      mergeA(1,learnDict[word5+"¶¶¶¶"].split(),
+      mergeA(1,learnDict[word4+"¶¶¶¶¶"].split(),
+      mergeA(1,learnDict[word3+"¶"].split(),
+      mergeA(1,learnDict[word2+"¶¶"].split(),
       learnDict[word1+"¶¶¶"].split())))))
 
-      word5 = random.choice(mergeA(learnDict[word5+"¶¶¶¶¶"].split(),
-      mergeA(learnDict[word4+"¶"].split(),
-      mergeA(learnDict[word3+"¶¶"].split(),
-      mergeA(learnDict[word2+"¶¶¶"].split(),
+      word5 = random.choice(
+      mergeA(1,learnDict[word5+"¶¶¶¶¶"].split(),
+      mergeA(2,learnDict[word4+"¶"].split(),
+      mergeA(3,learnDict[word3+"¶¶"].split(),
+      mergeA(4,learnDict[word2+"¶¶¶"].split(),
       learnDict[word1+"¶¶¶¶"].split())))))
 
       sentance = sentance+ word1+" "+word2+" "+word3+" "+word4+" "+word5+" "
+      print("Merge generated successfully.")
       j = j + 1
-    succsess = True
+    storyDone = True
   except:
-    print("Error handled.")
     succsess = False
 
-print("Data Size: ", dataLen/1000, "KB")
+print("Finished!! Data Size: ", dataLen/1000, "KB")
 print(sentance)
